953e044d6a4508706969fccdc993be891c1f86fe
Aub <aubhon.a@gmail.com>
2024-04-01 00:54:42 +0300

    Add ui features

--- TimeManager/Views/CustomViews/PopUpPresentationController.swift
+++ TimeManager/Views/CustomViews/PopUpPresentationController.swift
@@ -9,6 +9,45 @@ import UIKit
 
 internal final class PopUpPresentationController: UIPresentationController {
     public var divider = 2.5
+    private var dimmingView: UIView!
+    
+    override init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
+        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
+        configureDimmingView()
+    }
+    
+    private func configureDimmingView() {
+        dimmingView = UIView()
+        dimmingView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        dimmingView.alpha = 0.0
+    }
+    
+    override func presentationTransitionWillBegin() {
+        guard let containerView = containerView else { return }
+        containerView.insertSubview(dimmingView, at: 0)
+        
+        dimmingView.pin(to: containerView)
+        
+        guard let coordinator = presentedViewController.transitionCoordinator else {
+            dimmingView.alpha = 1.0
+            return
+        }
+        
+        coordinator.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 1.0
+        }, completion: nil)
+    }
+    
+    override func dismissalTransitionWillBegin() {
+        guard let coordinator = presentedViewController.transitionCoordinator else {
+            dimmingView.alpha = 0.0
+            return
+        }
+        
+        coordinator.animate(alongsideTransition: { _ in
+            self.dimmingView.alpha = 0.0
+        }, completion: nil)
+    }
     
     override var frameOfPresentedViewInContainerView: CGRect {
         guard let containerViewBounds = containerView?.bounds else { return .zero }
@@ -16,4 +55,3 @@ internal final class PopUpPresentationController: UIPresentationController {
         return CGRect(x: 0, y: containerViewBounds.height - height, width: containerViewBounds.width, height: height)
     }
 }
-
